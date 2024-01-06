abe0806779224bf4320a6667dda3ba4d4ce587f2
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-01-06 16:01:59 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,2 +1,4 @@
 MODDIR="${0%/*}"
+
 mount -t overlay -o lowerdir=/system/etc,upperdir=$MODDIR/system/etc,workdir=$MODDIR/worker KSU /system/etc
+mount -t overlay -o lowerdir=/system/bin,upperdir=$MODDIR/system/bin,workdir=$MODDIR/worker KSU /system/bin
