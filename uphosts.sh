f26644a5200ac3cf6e48b1a3c815f10f918f0e99
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-01-07 14:38:01 +0100

    Update uphosts.sh

--- uphosts.sh
+++ uphosts.sh
@@ -14,12 +14,12 @@ Choose your hosts file:
 PS3="Enter a number: "
 select option in Help Empty Basic Spark Full Custom Cleanup Cancel
 do
-echo "Selected option: $option"
-if [[ $REPLY =~ ^[2-8]$ ]]; then
-  break
-else
-  if [[ $REPLY == 1 ]]; then
-    echo "
+  echo "Selected option: $option"
+  if [[ $REPLY =~ ^[2-8]$ ]]; then
+    break
+  else
+    if [[ $REPLY == 1 ]]; then
+      echo "
 ------------------------------------
 Help  - Show this message
 Empty - Clear Hosts / Disable Hosts
@@ -38,36 +38,35 @@ Cancel  - Cancel this script
     else
       echo "${NUM} is not a valid option..."
     fi
-fi
+  fi
 done
 
 case $option in
 Help)
-cancel=true
+  cancel=true
 ;;
 Empty)
-empty=true
+  empty=true
 ;;
 Basic)
-link=https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  link=https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 ;;
 Spark)
-link=https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+  link=https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
 ;;
 Full)
-link=https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
+  link=https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
 ;;
-
 Custom)
-clear
-select coption in Help Single Addon Hosts Cancel
-do
-echo "Selected option: $coption"
-if [[ $REPLY =~ ^[2-5]$ ]]; then
-  break
-else
-  if [[ $REPLY == 1 ]]; then
-    echo "
+  clear
+  select coption in Help Single Addon Hosts Cancel
+  do
+    echo "Selected option: $coption"
+    if [[ $REPLY =~ ^[2-5]$ ]]; then
+      break
+    else
+      if [[ $REPLY == 1 ]]; then
+        echo "
 ------------------------------------
 Help   - Show this message
 Single - Add a single host
@@ -78,65 +77,66 @@ Hosts - Replace hosts file
 Cancel - Cancel this script
 ------------------------------------
 "
-    else
-      echo "${NUM} is not a valid option..."
+        else
+          echo "${NUM} is not a valid option..."
+        fi
     fi
-fi
-done
-case $coption in
-Help)
-cancel=true;
-;;
-Hosts)
-echo "
+  done
+  case $coption in
+  Help)
+    cancel=true;
+  ;;
+  Hosts)
+    echo "
 Paste your raw hosts link below:"
-read link
-;;
-Addon)
-echo "
+    read link
+  ;;
+  Addon)
+    echo "
 Paste your raw addon link below:"
-read link
-echo "" >> $hostsfile
-curl -fs $link >> $hostsfile
-cancel=true
-;;
-Single)
-echo "
+    read link
+    echo "" >> $hostsfile
+    curl -fs $link >> $hostsfile
+    cancel=true
+  ;;
+  Single)
+    echo "
 # Custom hosts below
 " >> $hostsfile
-echo "
+    echo "
 Enter your host below (Nothing - Quit):"
-while true
-do
-  read host
-  if [[ -z $host ]]; then
-    break
-  fi
-  echo "0.0.0.0 $host" >> $hostsfile
-done
-cancel=true
-;;
-Cleanup)
-sed -i 's/#.*$//' $hostsfile # Remove comments
-sed -i 's/\s*$//' $hostsfile # Remove blanks at end
-sed -i 's/^\s*//' $hostsfile # Remove blanks at beginning
-sed -i '/^\s*$/d' $hostsfile # Remove empty lines
-sort -u -o $hostsfile{,}     # Remove duplicates
-;;
-Cancel)
-cancel=true
-;;
-*)
-cancel=true
-;;
-esac
+    while true
+    do
+      read host
+      if [[ -z $host ]]; then
+        break
+      fi
+      echo "0.0.0.0 $host" >> $hostsfile
+    done
+    cancel=true
+  ;;
+  Cancel)
+    cancel=true
+  ;;
+  *)
+    cancel=true
+  ;;
+  esac
 ;;
 
+Cleanup)
+  sed -i 's/#.*$//' $hostsfile # Remove comments
+  sed -i 's/\s*$//' $hostsfile # Remove blanks at end
+  sed -i 's/^\s*//' $hostsfile # Remove blanks at beginning
+  sed -i '/^\s*$/d' $hostsfile # Remove empty lines
+  sort -u -o $hostsfile{,}     # Remove duplicates
+  cancel=true
+;;
 Cancel)
-cancel=true
+  cancel=true
 ;;
 *)
-cancel=true
+  cancel=true
 ;;
 esac
 
